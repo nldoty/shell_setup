beef8a0933dd7fcd24c1287ae4363d5bb39d0376
Nick Doty <doty.nick@gmail.com>
2020-05-14 22:28:13 -0400

    Add initial setup of oh my zsh and powerlevel10k

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#Variables
+ZSH_LOC="${ZSH_LOC:-$HOME/.oh-my-zsh}"
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+
+# Silence pushd/popd
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd > /dev/null
+}
+
+# Get Script Directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "$DIR" || exit
+
+echo "Checking for oh-my-zsh install"
+if [[ -d "$HOME/.oh-my-zsh" ]]; then
+  echo "Oh My Zsh already installed"
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+
+# Install powerlevel10 theme
+if [[-d "$ZSH_CUSTOM/themes/powerlevel10k"]] then
+    echo "Powerlevel10k theme already installed"
+else    
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+
+echo "Setting theme to powerlevel10k (remember to change the font in iTerm preferences)"
+sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k/powerlevel10k"/g' $HOME/.zshrc
+sed -i '' 's/plugins=(git)/plugins=(brew compleat git npm osx yarn)/g' $HOME/.zshrc
+sed -i '' 's/# HIST_STAMPS/HIST_STAMPS/g' $HOME/.zshrc
+sed -i '' 's/# DISABLE_UNTRACKED_FILES_DIRTY/DISABLE_UNTRACKED_FILES_DIRTY/g' $HOME/.zshrc
+sed -i '' 's/# COMPLETION_WAITING_DOTS/COMPLETION_WAITING_DOTS/g' $HOME/.zshrc
